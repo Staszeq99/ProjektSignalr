0760818924c805541f1c0b83b105888ea60d9c78
Dawid Stachowiak <49553238+Staszeq99@users.noreply.github.com>
2019-06-16 18:27:31 +0200

    Add files via upload

---
+++ DrawDot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Rysowanko
+{
+    public class DrawDot : Hub
+    {
+       public void UpdateCanvas(int x, int y)
+        {
+            Clients.All.updateDot(x, y);
+        }
+        public void ClearCanvas()
+        {
+            Clients.All.clearCanvas();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Rysowanko.Startup))]
+
+namespace Rysowanko
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Aby uzyskać więcej informacji o sposobie konfigurowania aplikacji, odwiedź stronę https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
